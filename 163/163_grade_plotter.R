3b0614eea882e2d21184d88ea239d3536444f427
Michael S. Taylor (home) <mtaylor@semo.edu>
2022-02-15 09:57:39 -0600

    Fall 2021

--- 163/163_grade_plotter.R
+++ 163/163_grade_plotter.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(ggthemes)
-library(extrafont)
+library(showtext) # Uses OTF files.
+#library(extrafont)
+
 
 # Registers ttf fonts for use with PDF output (ggsave), 
 # enables them to be saved in PDF file (embed_fonts).
@@ -8,8 +10,8 @@ library(extrafont)
 
 #Change exam name and numbers for each grade.
 # Number is order from A to F.
-exam_name <- "exam3_f19.pdf"
-Number <- c(12,17,12,2,1)
+exam_name <- "exam3_f21.pdf"
+Number <- c(5,11,7,6,7)
 
 Grade <- c("A","B","C","D","F")
 df <- data.frame(Grade, Number)
@@ -18,6 +20,9 @@ df <- data.frame(Grade, Number)
 #loadfonts(device = "pdf")
 
 # Build plot
+font_add(family = "Linux Biolinum", regular = "/Library/fonts/LinBiolinum_R.otf")
+showtext_auto()
+
 grade_plot <- ggplot(df, aes(Grade,Number)) + 
   geom_col(fill = "#2171B5", # Fill color is Blue6 from Beamer lectures
            width=0.7) + 
@@ -29,6 +34,7 @@ grade_plot <- ggplot(df, aes(Grade,Number)) +
   scale_y_continuous(breaks=seq(0,40,5)) +
   theme(axis.ticks = element_blank(), 
         axis.ticks.length = unit(0, "mm"),
+#        text=element_text(size=16),
         text=element_text(size=16,  family="Linux Biolinum"),
         axis.text.x = element_text(margin = margin(b = 10))) +
   coord_flip() 
@@ -39,12 +45,17 @@ grade_plot <- ggplot(df, aes(Grade,Number)) +
 # Because the graph is flipped, use x.major_source instead of y.major_source
 # y_intercept <- ggplot_build(grade_plot)$layout$panel_ranges[[1]]$x.major_source # No longer works.
 # See update.
-y_intercept <- ggplot_build(grade_plot)$layout$coord$labels(ggplot_build(grade_plot)$layout$panel_params)[[1]]$x.major_source
-grade_plot <- grade_plot + geom_hline(yintercept = y_intercept, color="white", size =  0.25)
+
+# Updated Sep 2021 for ggplot2 version 3.x
+# y_intercept <- ggplot_build(grade_plot)$layout$coord$labels(ggplot_build(grade_plot)$layout$panel_params)[[1]]$x.major_source
+y_intercept <- ggplot_build(grade_plot)$layout$panel_params[[1]]$x.sec$breaks
+
+grade_plot <- grade_plot + geom_hline(yintercept = na.omit(y_intercept), color="white", size =  0.25)
 
 # Save the PDf file.
 ggsave(exam_name, plot=grade_plot, width=4.5, height=3)
 
 # Embed the Linux Biolinum font.
-embed_fonts(exam_name)
+# Not needed when using showtext package.
+#embed_fonts(exam_name)
 
